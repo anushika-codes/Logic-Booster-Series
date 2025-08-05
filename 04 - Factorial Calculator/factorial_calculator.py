51d68c451cc621af4c02c9af55784e71efa96b5e
Anushika <anushika10073@gmail.com>
2025-08-05 18:09:29 +0530

    Added Factorial Calculator using Python loop

--- 04 - Factorial Calculator/factorial_calculator.py
+++ 04 - Factorial Calculator/factorial_calculator.py
@@ -1,1 +1,17 @@
+# Factorial Calculator
+print("Factorial Calculator")
 
+# Get input from user
+num = int(input("Enter a non-negative number: "))
+
+# Factorial of negative number doesn't exist
+if num < 0:
+    print("❌ Factorial does not exist for negative numbers.")
+elif num == 0 or num == 1:
+    print(f"The factorial of {num} is 1")
+else:
+    factorial = 1
+    for i in range(2, num + 1):
+        factorial *= i
+    print(f"The factorial of {num} is {factorial}")
+  
