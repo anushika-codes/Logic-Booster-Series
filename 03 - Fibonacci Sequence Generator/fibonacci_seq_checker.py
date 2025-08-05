6d16d7fc54cacd2b64f2e23c214fdcd72d37e8a0
Anushika <anushika10073@gmail.com>
2025-08-05 18:06:56 +0530

    Add Fibonacci series script with input-based loop

--- 03 - Fibonacci Sequence Generator/fibonacci_seq_checker.py
+++ 03 - Fibonacci Sequence Generator/fibonacci_seq_checker.py
@@ -1,1 +1,22 @@
+# Fibonacci Sequence Generator
+print("Fibonacci Sequence Generator")
 
+# Get number of terms from user
+num = int(input("Enter how many terms you want: "))
+
+# First two terms
+a, b = 0, 1
+count = 0
+
+# Generate sequence
+if num <= 0:
+    print("Please enter a positive number.")
+elif num == 1:
+    print("Fibonacci sequence up to 1 term:")
+    print(a)
+else:
+    print("Fibonacci sequence:")
+    while count < num:
+        print(a, end=" ")
+        a, b = b, a + b
+        count += 1
