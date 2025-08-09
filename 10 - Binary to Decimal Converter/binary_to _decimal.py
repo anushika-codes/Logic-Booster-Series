a98198ff8c3f359ac217ac36cd16a9db72f6cf8d
Anushika <anushika10073@gmail.com>
2025-08-09 20:16:32 +0530

    Completed

--- 10 - Binary to Decimal Converter/binary_to _decimal.py
+++ 10 - Binary to Decimal Converter/binary_to _decimal.py
@@ -1,1 +1,11 @@
+# Binary to Decimal Converter
 
+# Taking binary input from the user
+binary_num = input("Enter a binary number: ")
+
+# Converting binary to decimal
+try:
+    decimal_num = int(binary_num, 2)  # base 2
+    print(f"The decimal value of {binary_num} is {decimal_num}")
+except ValueError:
+    print("Invalid input! Please enter only 0s and 1s.")
