d9a530ba931bf01c523420f82516ef0260ed2d63
Anushika <anushika10073@gmail.com>
2025-08-17 11:38:18 +0530

    Completed

---
+++ 24 - Count words in a sentence/word_count.py
@@ -0,0 +1,12 @@
+# Program: Count Words in a Sentence
+
+# Input
+sentence = input("Enter a sentence: ")
+
+# Split sentence into words
+words = sentence.split()
+
+# Count words
+word_count = len(words)
+
+print("The number of words in the sentence is:", word_count)
